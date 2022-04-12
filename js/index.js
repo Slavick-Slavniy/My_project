440b2c690c70639ba8e550e27933ae49ae7bda19
Slavick-Slavniy <103335455+Slavick-Slavniy@users.noreply.github.com>
2022-04-12 22:54:32 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -46,37 +46,38 @@
 
 // Яким буде результат виконання коду?
 
-// ```js
-// console.log(5 > 4);
+// console.log(5 > 4); // true
 
-// console.log(10 >= '7');
+// console.log(10 >= '7'); // true
 
-// console.log('2' > '12');
+// console.log('2' > '22'); // false
 
-// console.log('2' < '12');
+// console.log('2' < '12'); // false
 
-// console.log('4' == 4);
+// console.log('4' == 4); // true
 
-// console.log('6' === 6);
+// console.log('6' === 6); // fals
 
-// console.log('false' === false);
+// console.log('false' === false); // false
 
-// console.log(1 == true);
+// console.log(1 == true); // true
 
-// console.log(1 === true);
+// console.log(1 === true); // false
 
-// console.log('0' == false);
+// console.log('0' == false); // true
 
-// console.log('0' === false);
+// console.log('0' === false); // false
 
-// console.log('Papaya' < 'papaya');
+// console.log('Papaya' < 'papaya'); // true
 
-// console.log('Papaya' === 'papaya');
+// console.log('Papaya' === 'papaya'); // false
 
-// console.log(undefined == null);
+// console.log(undefined == null); // true
 
-// console.log(undefined === null);
-// ```
+// console.log(undefined === null); // false
+
+// console.log(NaN === NaN); // false
+
 
 // ## Example 7 - Опертор % та методы рядків
 
@@ -99,6 +100,7 @@
 // const doubleDigitMinutes = String(minutes).padStart(2, 0);
 // console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
 // ```
+
 // ## Example 8 - Логічні оператори
 
 // Яким буде результат виразу
