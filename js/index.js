fd3ae72407e448801089555b7d1ca487d7db6637
Slavick-Slavniy <103335455+Slavick-Slavniy@users.noreply.github.com>
2022-04-13 12:03:46 +0300

    Summary

--- js/index.js
+++ js/index.js
@@ -168,7 +168,7 @@
 //     return a + b + c;
 //   // Change code above this line
 // }
-// add(2 + 5 + 8);
+// add(2, 5, 8); //15
 
 // let rezult = add(15, 27, 10); // 52
 // console.log(rezult)
@@ -193,6 +193,18 @@
 // console.log(add(15, 27, 10));
 // console.log(add(10, 20, 30));
 // console.log(add(5, 10, 15));
+
+// Завдання 10
+function makeMessage (name, price) {
+  // Change code below this line
+   const message = `"You picked ${name}, price per item is ${price} credits"`;
+  // Change code above this line
+  return message;
+};
+console.log(makeMessage('Radar', 6150));
+console.log(makeMessage('Scanner', 3500));
+console.log(makeMessage('Reactor', 8000));
+console.log(makeMessage('Enginer', 4070));
 
 
 
