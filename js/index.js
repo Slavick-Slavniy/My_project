80f0be0cea6a5f66ce74c1cddc54b6f8653e0986
Slavick-Slavniy <103335455+Slavick-Slavniy@users.noreply.github.com>
2022-04-13 12:16:44 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -195,16 +195,16 @@
 // console.log(add(5, 10, 15));
 
 // Завдання 10
-function makeMessage (name, price) {
-  // Change code below this line
-   const message = `"You picked ${name}, price per item is ${price} credits"`;
-  // Change code above this line
-  return message;
-};
-console.log(makeMessage('Radar', 6150));
-console.log(makeMessage('Scanner', 3500));
-console.log(makeMessage('Reactor', 8000));
-console.log(makeMessage('Enginer', 4070));
+// function makeMessage (name, price) {
+//   // Change code below this line
+//    const message = `"You picked ${name}, price per item is ${price} credits"`;
+//   // Change code above this line
+//   return message;
+// };
+// console.log(makeMessage('Radar', 6150));
+// console.log(makeMessage('Scanner', 3500));
+// console.log(makeMessage('Reactor', 8000));
+// console.log(makeMessage('Enginer', 4070));
 
 
 
