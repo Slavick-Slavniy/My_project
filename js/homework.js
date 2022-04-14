8a9807249ba1113c6921afe77d397e35d98ad99a
Slavick-Slavniy <103335455+Slavick-Slavniy@users.noreply.github.com>
2022-04-14 18:17:34 +0300

    Update homework.js

--- js/homework.js
+++ js/homework.js
@@ -314,25 +314,60 @@
 // console.log(getSubscriptionPrice("starter"));
 
 //Завдання 28
-function checkPassword(password) {
-  const ADMIN_PASSWORD = "jqueryismyjam";
-  let message;
-  // Change code below this line
-switch (password){
-  case null:
-  message = "Canceled by user!";
-  break;
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   let message;
+//   // Change code below this line
+// switch (password) {
+//   case null:
+//   message = "Canceled by user!";
+//   break;
 
-  case ADMIN_PASSWORD:
-  message = "Welcome!";
-  break;
+//   case "jqueryismyjam":
+//   message = "Welcome!";
+//   break;
 
-  default: 
-  message = "Access denied, wrong password!";
-}
-  return message;
-}
-console/log(checkPassword("mangohackzor"));
-console/log(checkPassword((null));
-console/log(checkPassword("polyhax"));
-console/log(checkPassword("jqueryismyjam"));
+//   default:
+//     message = "Access denied, wrong password!";
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+// console.log(checkPassword("mangohackzor"));
+// console.log(checkPassword(null));
+// console.log(checkPassword("polyhax"));
+// console.log(checkPassword("jqueryismyjam"));
+
+//Завдання 29
+// function getShippingCost(country) {
+//   let message;
+//   // Change code below this line
+//   switch (country) {
+//     case "China":
+//       message = "Shipping to China will cost 100 credits";
+//       break;
+//     case "Chile":
+//       message = "Shipping to Chile will cost 250 credits";
+//       break;
+//     case "Australia":
+//       message = "Shipping to Australia will cost 170 credits";
+//       break;
+//     case "Jamaica":
+//       message = "Shipping to Jamaica will cost 120 credits";
+//       break;
+//     default:
+//       message = "Sorry, there is no delivery to your country";
+//       break;
+//   }
+//   // Change code above this line
+//   return message;
+// }
+// console.log(getShippingCost("Australia"));
+// console.log(getShippingCost("Germany"));
+// console.log(getShippingCost("China"));
+// console.log(getShippingCost("Chile"));
+// console.log(getShippingCost("Jamaica"));
+// console.log(getShippingCost("Sweden"));
+
+//Завдання 30
