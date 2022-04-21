641df45101ac696fe321cb137b3ca198cb3d6d2c
Slavick-Slavniy <103335455+Slavick-Slavniy@users.noreply.github.com>
2022-04-21 16:36:04 +0300

    Update homework.js

--- js/homework.js
+++ js/homework.js
@@ -657,3 +657,47 @@
 // console.log(createArrayOfNumbers(1, 3));
 // console.log(createArrayOfNumbers(14, 17));
 // console.log(createArrayOfNumbers(29, 34));
+
+// // Завдання 23
+// function filterArray(numbers, value) {
+//    // Change code below this line
+//     let valueNumbers = [];
+//     for (let i = 0; i < numbers.length; i += 1){
+//         if (numbers[i] > value) {valueNumbers.push(numbers[i])}
+//     }
+// return valueNumbers
+
+//   // Change code above this line
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// Завдання 24
+// function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"];
+//    return fruits.includes(fruit);
+    
+//    // Change this line
+// }
+// console.log(checkFruit("plum"));
+// console.log(checkFruit("mandarin"));
+// console.log(checkFruit("pear"));
+// console.log(checkFruit("Pear"));
+// console.log(checkFruit("apple"));
+
+// Завдання 25
+function getCommonElements(array1, array2) {
+    // Change code below this line
+    let newArray = [];
+    for (let i = 1; i < array1.length; i += 1) {
+        if (array1[i] >= array2[i]) {)};
+    
+
+        return newArray
+        // Change code above this line
+    }
+}
+console.log(getCommonElements([1, 2, 3], [2, 4]));
