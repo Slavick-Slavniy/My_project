17ea45032630ff4b4ae0f31aa24e74eeb1a7f141
Slavick-Slavniy <103335455+Slavick-Slavniy@users.noreply.github.com>
2022-04-23 02:20:33 +0300

    Update homework.js

--- js/homework.js
+++ js/homework.js
@@ -692,7 +692,7 @@
 // function getCommonElements(array1, array2) {
 //   // Change code below this line
 //     let newArray = [];
-//     for (let i = 0; i < array1.length; i += 1) 
+//     for (let i = 0; i < array1.length; i += 1)
 //         if (array2.includes(array1[i]))
         
 //         { newArray.push(array1[i]) }
@@ -707,21 +707,116 @@
 // console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
 // console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
 
+// Завдання 26
+// function calculateTotalPrice(order) {
+//     let total = 0;
+//     // Change code below this line
+//     for (let i of order) {
+//         total += i;
+//   }
+//   // Change code above this line
+//     return total;
+// }
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+// console.log(calculateTotalPrice([]));
+    
+//Завдання 27
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const filteredNumbers = [];
+//         for (const number of numbers) {
+//       if (number > value) {
+//       filteredNumbers.push(number);
+//   }
+//   }
+// return filteredNumbers;
+//   // Change code above this line
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
 
+//Завдання 28
+// Change code below this line
+// const a = 3 % 3;
+// const b = 4 % 3;
+// const c = 11 % 4 ;
+// const d = 12 % 7;
+// const e = 8 % 6;
 
+// Завдання 29
+// function getEvenNumbers(start, end) {
+//    // Change code below this line
+//       let newArray = [];
+//     for (let i = start; i <= end; i += 1)
+//         if (i % 2 === 0)
+//     { newArray.push(i) }
+    
+//     return newArray
 
+//     // Change code above this line
+//   }
+// console.log(getEvenNumbers(2, 5));
+// console.log(getEvenNumbers(3, 11));
+// console.log(getEvenNumbers(6, 12));
+// console.log(getEvenNumbers(8, 8));
+// console.log(getEvenNumbers(7, 7));
 
+// Завдання 30
+// const start = 6;
+// const end = 27;
+// let number;
 
+// for (let i = start; i <= end; i += 1) {
+//    if (i % 5 === 0) {
+//         number = i;
+//        break;
+//     }
+  
+// }
+// console.log(number);
 
+// Завдання 31
+// function findNumber(start, end, divisor) {
+//   // Change code below this line
+//   let number ;
 
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+//       return i
+//       number = i;
+      
+//     }
+//   }
 
+//   return number;
+//   // Change code above this line
+// }
+// console.log(findNumber(2, 6, 5));
+// console.log(findNumber(8, 17, 3));
+// console.log(findNumber(6, 9, 4));
+// console.log(findNumber(16, 35, 7));
 
-
-
-
-
-
-
-
-
-
+// Завдання 32
+// function includes(array, value) {
+//     // Change code below this line
+//     let valueArray;
+//     for (let i = 0; i < array.length; i += 1) {
+//         if (valueArray = (value === array[i])) { break
+//         }
+        
+//         // Change code above this line
+//     }
+//     return valueArray
+// }   
+// console.log(includes([1, 2, 3, 4, 5], 3));
+// console.log(includes([1, 2, 3, 4, 5], 17));
+// console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
+// console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
+// console.log((["apple", "plum", "pear", "orange"], "plum"));
+// console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
+    
